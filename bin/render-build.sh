5bb4524cb3bdbdfbe4214bb2b252047686357220
tokunaga3 <mutsukitokunaga@outlook.jp>
2023-11-12 15:22:51 +0900

    database.yml修正

--- bin/render-build.sh
+++ bin/render-build.sh
@@ -5,6 +5,7 @@ set -o errexit
 mkdir -p ./tmp/sockets
 # cron
 # bundle exec whenever --update-crontab
+bundle exec rake  db:create
 bundle exec rake  db:migrate
 # bundle exec rake db:seed
 bundle exec puma -C config/puma.rb
