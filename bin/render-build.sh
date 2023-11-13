0816fe9da13be2c94c8702ea7c21eb446619c67d
tokunaga3 <mutsukitokunaga@outlook.jp>
2023-11-13 22:18:57 +0900

    render-build修正

--- bin/render-build.sh
+++ bin/render-build.sh
@@ -2,13 +2,8 @@
 # exit on error
 set -o errexit
 
-# rm -f tmp/pids/server.pid
-# tail -f /dev/null
-# cron
-# bundle exec whenever --update-crontab
-bundle exec rake  db:migrate
-# bundle exec rake db:seed
-# mkdir -p ./tmp/sockets
-pwd
-# bundle exec puma -C config/puma.rb
+bundle install
+bundle exec rake assets:precompile
+bundle exec rake assets:clean
+bundle exec rake db:migrate
 exec "$@"
