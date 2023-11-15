74a914a064b8b1be4dc6ba57ed06007aa13eafc4
tokunaga3 <mutsukitokunaga@outlook.jp>
2023-11-15 21:35:11 +0900

    bin/webpackした

--- bin/render-build.sh
+++ bin/render-build.sh
@@ -2,9 +2,9 @@
 # exit on error
 set -o errexit
 bundle install
-bundle exec rake assets:precompile
-bundle exec rake assets:clean
-bundle exec vite upgrade
+# bundle exec rake assets:precompile
+# bundle exec rake assets:clean
+bin/vite dev
 bundle exec rake db:migrate
 bundle exec puma -C config/puma.rb
 
