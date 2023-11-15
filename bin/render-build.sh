53772d6142352af09cad9b2abd198a493bd75f50
tokunaga3 <mutsukitokunaga@outlook.jp>
2023-11-15 21:13:21 +0900

    bin/webpackした

--- bin/render-build.sh
+++ bin/render-build.sh
@@ -4,6 +4,8 @@ set -o errexit
 bundle install
 bundle exec rake assets:precompile
 bundle exec rake assets:clean
+rm -rf babelrc
+bin/webpack
 bundle exec rake db:migrate
 bundle exec puma -C config/puma.rb
 
